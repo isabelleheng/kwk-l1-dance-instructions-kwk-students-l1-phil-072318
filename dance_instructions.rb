6905a9b6c2d918cb5e8bf4988f72a1f27154a373
isabelleheng <isabellejhheng@gmail.com>
2018-07-24 15:13:50 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,40 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart"
+end
+
+def bust_a_move
+  puts "Be cool and be snazzy"
+end
+
+def base_footwork
+  puts "Lift"
+  puts "Return"
+  puts "Finish"
+end
+
+def skip_step(left)
+  puts "Lower"
+  puts "Bouce"
+  puts "Briefly"
+end
+def skip_step(right)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+def bob_the_reins
+  puts "Hold"
+  puts "Cross"
+  puts "Lift"
+  puts "Do"
+end
+
+def lasso 
+  puts "Start"
+  puts "Lift"
+  puts "Raise"
+  puts "Do"
+end 
